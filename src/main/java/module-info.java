10621666d36ab7bb6cd672319e98bd16352cb782
Abubakar Kashif <abubakarkashif@macbookair.home>
2025-02-18 00:51:20 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module universitymanagementsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens universitymanagementsystem to javafx.fxml;
+    exports universitymanagementsystem;
+
+}
